84e7854bccce344bb69698a94158a6a2c0b95882
Akhilesh <akhileshpujar796@gmail.com>
2024-02-04 18:59:23 +0530

    openapi-ignored

--- lib/fetchSuggestion.ts
+++ lib/fetchSuggestion.ts
@@ -3,7 +3,7 @@ import formatTodosForAI from "./formatTodosForAI";
 
 const fetchSuggestion = async (board: Board) => {
 
-    const apiKey = "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"
+    const apiKey = process.env.OPENAI_API_KEY!
     const todos = formatTodosForAI(board);  //because sometimes ai mess up the format and we dont want that 
     console.log("formated todo to send>>", todos)
     const res = await fetch("/api/generateSummary", {
